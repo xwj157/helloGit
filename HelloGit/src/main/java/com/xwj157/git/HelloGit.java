c7ebfb948172acc95d3df8c6741dbe251611b758
xwj157 <1085496783@qq.com>
2020-10-02 22:22:25 +0800

    创建分支

--- HelloGit/src/main/java/com/xwj157/git/HelloGit.java
+++ HelloGit/src/main/java/com/xwj157/git/HelloGit.java
@@ -7,5 +7,7 @@ package com.xwj157.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("update");
+        System.out.println("创建分支");
     }
 }
