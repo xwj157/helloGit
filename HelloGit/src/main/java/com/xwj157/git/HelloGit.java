e550d0b147f280682db8b132253ead177380d930
xwj157 <1085496783@qq.com>
2020-10-02 22:00:49 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/xwj157/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.xwj157.git;
+
+/**
+ * @author xwj157
+ * @create 2020-10-02 21:44
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+}
